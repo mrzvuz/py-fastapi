46e4acb8d00395987707967c83514e704da61c35
Ali <mrzv.uz@gmail.com>
2026-07-22 19:06:10 +0300

    Added get, post, put and delete API request, added error handling, and applied data validation with Pydantic

--- books.py
+++ books.py
@@ -1,36 +1,6 @@
-from typing import Optional
-from fastapi import FastAPI, Path, Query, HTTPException, Body
-from pydantic import BaseModel, Field
-from starlette import status
+from fastapi import FastAPI, HTTPException, Body
 
 app = FastAPI()
-
-
-# class Book:
-#     id: int
-#     title: str
-#     author: str
-#     description: str
-#     rating: int
-#     published_date: int
-
-#     def __init__(self, id, title, author, description, rating, published_date):
-#         self.id = id
-#         self.title = title
-#         self.author = author
-#         self.description = description
-#         self.rating = rating
-#         self.published_date = published_date
-
-
-# BOOKS = [
-#     Book(1, "Computer Science Pro", "codingwithroby", "A very nice book!", 5, 2030),
-#     Book(2, "Be Fast with FastAPI", "codingwithroby", "A great book!", 5, 2030),
-#     Book(3, "Master Endpoints", "codingwithroby", "A awesome book!", 5, 2029),
-#     Book(4, "HP1", "Author 1", "Book Description", 2, 2028),
-#     Book(5, "HP2", "Author 2", "Book Description", 3, 2027),
-#     Book(6, "HP3", "Author 3", "Book Description", 1, 2026),
-# ]
 
 
 BOOKS = [
@@ -64,6 +34,15 @@ async def read_category_by_query(category: str):
     return book_to_return
 
 
+@app.get("/books/byauthor/{author}")
+async def read_books_by_author(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("author").casefold() == author.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
 @app.get("/books/{book_author}")
 async def read_author_category_by_query(book_author: str, category: str):
     books_to_return = []
@@ -79,3 +58,21 @@ async def read_author_category_by_query(book_author: str, category: str):
 @app.post("/books/create_book")
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i, book in enumerate(BOOKS):
+        if book.get("title").casefold() == updated_book.get("title").casefold():
+            BOOKS[i] = updated_book
+            return {"message": "Book updated"}
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i, book in enumerate(BOOKS):
+        if book.get("title").casefold() == book_title.casefold():
+            del BOOKS[i]
+            return {"message": "Book deleted successfully."}
+
+    raise HTTPException(status_code=404, detail="Book not found.")
